34bad63f4be5c9fead0e3ea36806f55547239360
TonyGuan <si92@sina.com>
2014-11-13 15:16:59 +0800

    commit tonyguan

--- RioOlympics2016/AppDelegate.swift
+++ RioOlympics2016/AppDelegate.swift
@@ -2,9 +2,16 @@
 //  AppDelegate.swift
 //  RioOlympics2016
 //
-//  Created by tonymacmini on 14/11/5.
-//  Copyright (c) 2014年 tonyguan. All rights reserved.
 //
+//  Created by 关东升 on 2014-5-18.
+//  本书网站：http://www.cocoagame.net
+//  智捷iOS课堂在线课堂：http://v.51work6.com
+//  智捷iOS课堂新浪微博：http://weibo.com/u/3215753973
+//  作者微博：http://weibo.com/516inc
+//  QQ：1575716557 邮箱：jylong06@163.com
+//  QQ交流群：389037167/327403678
+//
+
 
 import UIKit
 
